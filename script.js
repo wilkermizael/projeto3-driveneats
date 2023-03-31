ce18a94c9d7b0134ff8851b3a01ce80133d26e87
Wilker <wilkermizael@gmail.com>
2023-03-31 17:11:32 -0300

    Selecionar prato,bebida e sobremesa finalizados

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-function selecionarPrato(param){
-    const divJaSelecionada = document.querySelector('.selecionado');
+
+const btn = false;
+
+function selecionarPrato(prato){
+    const divJaSelecionada = document.querySelector('.prato .selecionado');
     if(divJaSelecionada !== null){
         divJaSelecionada.classList.remove('selecionado');
+      
+    }
+        
+    const minhaDiv = document.querySelector(prato);
+    minhaDiv.classList.add('selecionado');
+
+
+}
+
+function selecionarBebida(bebida){
+    const divJa = document.querySelector('.bebida .selecionado');
+    if(divJa !== null){
+        divJa.classList.remove('selecionado');
     }
     
-    const minhaDiv = document.querySelector(param);
-    minhaDiv.classList.add('selecionado');
+    const minhaD = document.querySelector(bebida);
+    minhaD.classList.add('selecionado');
+
+    
 }
+
+function selecionarSobremesa(sobremesa){
+    const divSobremesa = document.querySelector('.sobremesa .selecionado');
+    if(divSobremesa !== null){
+        divSobremesa.classList.remove('selecionado');
+    }
+    
+    const minhaSobr = document.querySelector(sobremesa);
+    minhaSobr.classList.add('selecionado');
+
+    const cont = document.querySelector('.selecionado');
+    
+}
+

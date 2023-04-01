1d9aea904c781f82b0ccd6d5522fed68a47ff258
Wilker <wilkermizael@gmail.com>
2023-04-01 11:33:46 -0300

    Seleções e Check de todos os produtos

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 
-const btn = false;
 
-function selecionarPrato(prato){
+
+function selecionarPrato(prato, p1check){
     const divJaSelecionada = document.querySelector('.prato .selecionado');
+    const icone= document.querySelector('.prato .check');
+   
+    
     if(divJaSelecionada !== null){
         divJaSelecionada.classList.remove('selecionado');
-      
+        
     }
         
     const minhaDiv = document.querySelector(prato);
     minhaDiv.classList.add('selecionado');
+   
 
+    if( icone !== null){
+        icone.classList.remove('check')
+    }
 
+    const checkmark = document.querySelector(p1check);
+    checkmark.classList.add('check');
 }
 
-function selecionarBebida(bebida){
+function selecionarBebida(bebida, p2check){
     const divJa = document.querySelector('.bebida .selecionado');
+    const iconeBebida= document.querySelector('.bebida .check');
+
     if(divJa !== null){
         divJa.classList.remove('selecionado');
     }
@@ -23,11 +34,20 @@ function selecionarBebida(bebida){
     const minhaD = document.querySelector(bebida);
     minhaD.classList.add('selecionado');
 
+    if( iconeBebida !== null){
+        iconeBebida.classList.remove('check')
+    }
+
+    const checkmarkBebida = document.querySelector(p2check);
+    checkmarkBebida.classList.add('check');
+
     
 }
 
-function selecionarSobremesa(sobremesa){
+function selecionarSobremesa(sobremesa, p3check){
     const divSobremesa = document.querySelector('.sobremesa .selecionado');
+    const iconPudim = document.querySelector('.sobremesa .check');
+
     if(divSobremesa !== null){
         divSobremesa.classList.remove('selecionado');
     }
@@ -35,7 +55,12 @@ function selecionarSobremesa(sobremesa){
     const minhaSobr = document.querySelector(sobremesa);
     minhaSobr.classList.add('selecionado');
 
-    const cont = document.querySelector('.selecionado');
+    if( iconPudim !== null){
+        iconPudim.classList.remove('check');
+    }
+
+    const checkmarkPudim = document.querySelector(p3check);
+    checkmarkPudim.classList.add('check');
     
 }
 

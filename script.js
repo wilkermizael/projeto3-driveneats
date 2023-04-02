aac204cb2d55323ea0c70016b914d1b9b5e453ce
Wilker <wilkermizael@gmail.com>
2023-04-02 13:27:36 -0300

    Salva o cardápio escolhido e retorna valores

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let selectPrato = '';
 let selectBebida = '';
 let selectSobremesa = '';
+let meuPrato = " ";
+let minhaBebida ='';
+let minhaSobremesa = '';
+
+let valorPrato ='';
+let valorBebida ='';
+let valorSobremesa ='';
 
 function habilitarBotao(){
    
@@ -8,14 +15,18 @@ function habilitarBotao(){
         const botao = document.querySelector('.botao-falso');
         botao.classList.add('habilitar');
         const fecharPedido = document.querySelector('.botao-falso');
-        fecharPedido.innerHTML = 'Fechar Pedido'
+        fecharPedido.innerHTML = 'Fechar Pedido';
+        
+        document.querySelector('.botao-falso').removeAttribute('disabled');
+        /*calcular();*/
    }
    
 }
 
 function selecionarPrato(prato, p1check){
     
-   selectPrato =document.querySelector(p1check);
+   selectPrato =document.querySelector(prato);
+   
    
 
     const divJaSelecionada = document.querySelector('.prato .selecionado');
@@ -42,7 +53,7 @@ function selecionarPrato(prato, p1check){
 }
 
 function selecionarBebida(bebida, p2check){
-    selectBebida =document.querySelector(p2check);
+    selectBebida =document.querySelector(bebida);
     
     const divJa = document.querySelector('.bebida .selecionado');
     const iconeBebida= document.querySelector('.bebida .check');
@@ -65,7 +76,7 @@ function selecionarBebida(bebida, p2check){
 }
 
 function selecionarSobremesa(sobremesa, p3check){
-    selectSobremesa =document.querySelector(p3check);
+    selectSobremesa =document.querySelector(sobremesa);
     
     const divSobremesa = document.querySelector('.sobremesa .selecionado');
     const iconPudim = document.querySelector('.sobremesa .check');
@@ -88,3 +99,114 @@ function selecionarSobremesa(sobremesa, p3check){
     
 }
 
+function calcular(){
+    
+     // Selecionando o Prato escolhido e seu valor
+    
+    let selectPrato1 = document.querySelector('.prato1');
+    let selectPrato2 = document.querySelector('.prato2');
+    let selectPrato3 = document.querySelector('.prato3');
+    let selectPrato4 = document.querySelector('.prato4');
+    
+    
+   
+    if(selectPrato === selectPrato1){
+        meuPrato= document.querySelector('.prato1 h4').innerHTML;
+        valorPrato =document.querySelector('.prato1 h5').innerHTML;
+        console.log(meuPrato);
+        console.log(valorPrato);
+    }
+
+    if(selectPrato === selectPrato2){
+        meuPrato= document.querySelector('.prato2 h4').innerHTML;
+        valorPrato =document.querySelector('.prato2 h5').innerHTML;
+        console.log(meuPrato);
+        console.log(valorPrato);
+    }
+
+    if(selectPrato === selectPrato3){
+        meuPrato= document.querySelector('.prato3 h4').innerHTML;
+        valorPrato =document.querySelector('.prato3 h5').innerHTML;
+        console.log(meuPrato);
+        console.log(valorPrato);
+    }
+
+    if(selectPrato === selectPrato4){
+        meuPrato= document.querySelector('.prato4 h4').innerHTML;
+        valorPrato =document.querySelector('.prato4 h5').innerHTML;
+        console.log(meuPrato);
+        console.log(valorPrato);
+    }
+
+    // Escolhendo a Bebida
+
+    let selectBebida1 = document.querySelector('.bebida1');
+    let selectBebida2 = document.querySelector('.bebida2');
+    let selectBebida3 = document.querySelector('.bebida3');
+    let selectBebida4 = document.querySelector('.bebida4');
+
+    if(selectBebida === selectBebida1){
+        minhaBebida= document.querySelector('.bebida1 h4').innerHTML;
+        valorBebida= document.querySelector('.bebida1 h5').innerHTML;
+        console.log(minhaBebida);
+        console.log(valorBebida);
+    }
+
+    if(selectBebida === selectBebida2){
+        minhaBebida= document.querySelector('.bebida2 h4').innerHTML;
+        valorBebida= document.querySelector('.bebida2 h5').innerHTML;
+        console.log(minhaBebida);
+        console.log(valorBebida);
+    }
+
+    if(selectBebida === selectBebida3){
+        minhaBebida= document.querySelector('.bebida3 h4').innerHTML;
+        valorBebida= document.querySelector('.bebida3 h5').innerHTML;
+        console.log(minhaBebida);
+        console.log(valorBebida);
+    }
+
+    if(selectBebida === selectBebida4){
+        minhaBebida= document.querySelector('.bebida4 h4').innerHTML;
+        valorBebida= document.querySelector('.bebida4 h5').innerHTML;
+        console.log(minhaBebida);
+        console.log(valorBebida);;
+    }
+
+    // Escolhendo a Bebida
+
+    let selectSobremesa1 = document.querySelector('.sobremesa1');
+    let selectSobremesa2 = document.querySelector('.sobremesa2');
+    let selectSobremesa3 = document.querySelector('.sobremesa3');
+    let selectSobremesa4 = document.querySelector('.sobremesa4');
+
+    if(selectSobremesa === selectSobremesa1){
+        minhaSobremesa= document.querySelector('.sobremesa1 h4').innerHTML;
+        valorSobremesa= document.querySelector('.sobremesa1 h5').innerHTML;
+        console.log(minhaSobremesa);
+        console.log(valorSobremesa);
+    }
+
+    if(selectSobremesa === selectSobremesa2){
+        minhaSobremesa= document.querySelector('.sobremesa2 h4').innerHTML;
+        valorSobremesa= document.querySelector('.sobremesa2 h5').innerHTML;
+        console.log(minhaSobremesa);
+        console.log(valorSobremesa);
+    }
+
+    if(selectSobremesa === selectSobremesa3){
+        minhaSobremesa= document.querySelector('.sobremesa3 h4').innerHTML;
+        valorSobremesa= document.querySelector('.sobremesa3 h5').innerHTML;
+        console.log(minhaSobremesa);
+        console.log(valorSobremesa);
+    }
+
+    if(selectSobremesa === selectSobremesa4){
+        minhaSobremesa= document.querySelector('.sobremesa4 h4').innerHTML;
+        valorSobremesa= document.querySelector('.sobremesa4 h5').innerHTML;
+        console.log(minhaSobremesa);
+        console.log(valorSobremesa);
+    }
+
+    
+}
